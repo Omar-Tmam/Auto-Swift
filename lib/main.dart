a04c2f604850bc71df459a14c097b43426640171
omartmam <omartmam9@gmail.com>
2025-09-15 16:08:59 +0300

    create new app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:auto_swift/Core/utils/app_router.dart';
+import 'package:auto_swift/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const AutoSwift());
 }
 
