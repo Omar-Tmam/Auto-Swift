56dcc98b3a276933d6fb96075fc69de2b9564608
omartmam <omartmam9@gmail.com>
2025-09-15 15:00:10 +0300

    create mobile and tablet admin view

--- lib/Features/admin_view/presentation/views/admin_view.dart
+++ lib/Features/admin_view/presentation/views/admin_view.dart
@@ -1,3 +1,6 @@
+import 'package:auto_swift/Core/utils/custom_adaptive_layout.dart';
+import 'package:auto_swift/Features/admin_view/presentation/views/widgets/mobile/mobile_admin_view.dart';
+import 'package:auto_swift/Features/admin_view/presentation/views/widgets/tablet/tablet_admin_view.dart';
 import 'package:flutter/material.dart';
 
 class AdminView extends StatelessWidget {
@@ -5,6 +8,9 @@ class AdminView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomAdaptiveLayout(
+      mobileLayout: (context) => MobileLayoutAdminView(),
+      tabletLayout: (context) => TabletLayoutAdminView(),
+    );
   }
 }
---
+++ lib/Features/admin_view/presentation/views/widgets/mobile/mobile_admin_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MobileLayoutAdminView extends StatelessWidget {
+  const MobileLayoutAdminView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
