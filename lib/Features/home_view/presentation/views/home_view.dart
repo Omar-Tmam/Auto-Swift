5c459f970052008f907943f69e4d7cffa324aac2
omartmam <omartmam9@gmail.com>
2025-09-18 13:57:45 +0300

    create home view for tablet and mobile

---
+++ lib/Features/home_view/presentation/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:auto_swift/Core/utils/custom_adaptive_layout.dart';
+import 'package:auto_swift/Features/home_view/presentation/views/widgets/mobile/mobile_home_view.dart';
+import 'package:auto_swift/Features/home_view/presentation/views/widgets/tablet/tablet_home_view.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAdaptiveLayout(
+      mobileLayout: (context) => MobileHomeView(),
+      tabletLayout: (context) => TabletHomeView(),
+    );
+  }
+}
---
+++ lib/Features/home_view/presentation/views/widgets/tablet/tablet_home_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabletHomeView extends StatelessWidget {
+  const TabletHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
